398a26cacc39f240212387a7d2e9d1fddd046979
tolar07 <tolar07@users.noreply.github.com>
2026-09-07 04:20:16 +0100

    chore(auto): vault-memory sync + changes 2026-09-07 03:20:16

--- olpxdv_framework/src/olpxdv_framework/config/settings.py
+++ olpxdv_framework/src/olpxdv_framework/config/settings.py
@@ -192,7 +192,8 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False,
-        env_nested_delimiter="__"
+        env_nested_delimiter="__",
+        extra="allow"
     )
 
 
